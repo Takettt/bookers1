8c821e0bcb9163378484709c8293c7f0c587c8a9
EC2 Default User <ec2-user@ip-172-31-41-43.ap-northeast-1.compute.internal>
2023-10-16 12:18:00 +0000

    Finish

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
+  config.hosts << "65d1e058265640f6a6a99900556ea0fc.vfs.cloud9.ap-northeast-1.amazonaws.com"
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 end
